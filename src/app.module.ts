c8b5efe2f535eaca8fe65cbaad00faea901bfdfb
yamamoto244 <tsuyos.yamamoto@gmail.com>
2022-01-01 14:31:17 +0900

    feature: add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { UserService } from './services/user/user.service';
-
+import { BooksModule } from './books/books.module';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, UserService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
+import { BooksService } from './books.service';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('BooksService', () => {
+  let service: BooksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [BooksService],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    service = module.get<BooksService>(BooksService);
   });
 
   it('should be defined', () => {
